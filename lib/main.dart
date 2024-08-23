a51232a2ad65ec28970d32e87e4014545d87e68d
MFazla <2019t00441@stu.cmb.ac.lk>
2024-08-23 20:21:02 +0530

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:basic_crud_app/screens/splash_screen/splash_screen.dart';
-   void main() {
+import 'package:get/get.dart';
+import 'package:basic_crud_app/routes/app_route.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
+      getPages: appRoutes,
+      initialRoute: "/",
     );
   }
 }
---
+++ lib/routes/app_route.dart
@@ -0,0 +1,9 @@
+import 'package:basic_crud_app/screens/splash_screen/splash_screen.dart';
+import 'package:get/get.dart';
+
+var appRoutes = [
+  GetPage(
+    name: "/",
+    page: () => SplashScreen(),
+  ),
+];
