fef2c0ea08523a0d613e195b833f9b2a7017bb5d
MFazla <2019t00441@stu.cmb.ac.lk>
2024-08-26 12:07:18 +0530

    login routing

--- lib/controllers/splash_screen/splash_screen_controller.dart
+++ lib/controllers/splash_screen/splash_screen_controller.dart
@@ -1,16 +1,21 @@
 import 'package:get/get.dart';
-
 class SplashScreenController extends GetxController {
   var opacity = (0.0).obs;
-
   var scale = (0.0).obs;
-
   @override
   void onInit() {
     super.onInit();
     Future.delayed(const Duration(seconds: 1), () {
       opacity.value = 1;
       scale.value = 1;
+      Future.delayed(const Duration(seconds: 2), () {
+        navigateToNextScreen();
+      });
     });
+  }
+
+  // navigate to login screen or home screen
+  void navigateToNextScreen() {
+    Get.offNamed("/login-screen");
   }
 }
--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,3 +1,4 @@
+import 'package:basic_crud_app/screens/login_screen/login_screen.dart';
 import 'package:basic_crud_app/screens/splash_screen/splash_screen.dart';
 import 'package:get/get.dart';
 
@@ -5,5 +6,9 @@ var appRoutes = [
   GetPage(
     name: "/",
     page: () => SplashScreen(),
+  ),
+  GetPage(
+    name: "/login-screen",
+    page: () => LoginScreen(),
   ),
 ];
